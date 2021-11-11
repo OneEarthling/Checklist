f8ac93840999f671fe29761b6b7c0e720eba44e3
nadya <kazynkina@keysystems.ru>
2021-11-11 16:17:08 +0300

    Initial Commit

---
+++ Checklist/ChecklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChecklistApp.swift
+//  Checklist
+//
+//  Created by Nadya K on 28.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ChecklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
